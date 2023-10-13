4101bff787bfa287ab30f5554ea6446c79ee77c7
Pijar Pahlawan <pijarpahlawan@students.amikom.ac.id>
2023-10-13 16:38:26 +0700

    update data source

---
+++ src/scripts/data/api-endpoint.js
@@ -0,0 +1,5 @@
+const API_ENDPOINTS = {
+    RESTAURANT_LIST: "./data/DATA.json",
+};
+
+export default API_ENDPOINTS;
--- src/scripts/data/data-source.js
+++ src/scripts/data/data-source.js
@@ -1,6 +1,8 @@
+import API_ENDPOINTS from "./api-endpoint";
+
 async function getAllRestaurant() {
     try {
-        const response = await fetch("./data/DATA.json");
+        const response = await fetch(API_ENDPOINTS.RESTAURANT_LIST);
         const responseJson = await response.json();
         return responseJson.restaurants;
     } catch (error) {
