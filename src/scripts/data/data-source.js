8006810e29c785b37b552cd3c1d103d0ee6d76f1
Pijar Pahlawan <pijarpahlawan@students.amikom.ac.id>
2023-10-13 14:21:42 +0700

    removing template menu list

--- src/scripts/data/data-source.js
+++ src/scripts/data/data-source.js
@@ -1,18 +1,6 @@
-import API_ENDPOINTS from "./api-endpoints";
-
-async function getAllMenu() {
-    try {
-        const response = await fetch(API_ENDPOINTS.MENU_LIST);
-        const responseJson = await response.json();
-        return responseJson.meals;
-    } catch (error) {
-        return error.message;
-    }
-}
-
 async function getAllRestaurant() {
     try {
-        const response = await fetch(API_ENDPOINTS.RESTAURANT_LIST);
+        const response = await fetch("./data/DATA.json");
         const responseJson = await response.json();
         return responseJson.restaurants;
     } catch (error) {
@@ -20,4 +8,4 @@ async function getAllRestaurant() {
     }
 }
 
-export { getAllMenu, getAllRestaurant };
+export { getAllRestaurant };
