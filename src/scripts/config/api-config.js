dd426099e9308e333942b760821bc69d323cc470
Pijar Pahlawan <pijarpahlawan@students.amikom.ac.id>
2023-10-14 10:01:35 +0700

    add global config

---
+++ src/scripts/config/api-config.js
@@ -0,0 +1,7 @@
+const API_CONFIG = {
+    BASE_URL: "https://restaurant-api.dicoding.dev/",
+    BASE_SMALL_IMAGE_URL: "https://restaurant-api.dicoding.dev/images/small/",
+    BASE_MEDIUM_IMAGE_URL: "https://restaurant-api.dicoding.dev/images/medium/",
+};
+
+export default API_CONFIG;
---
+++ src/scripts/config/db-config.js
@@ -0,0 +1,7 @@
+const DB_CONFIG = {
+    DB_NAME: "restaurant-app-database",
+    DB_VERSION: 1,
+    OBJECT_STORE_NAME: "restaurants",
+};
+
+export default DB_CONFIG;
