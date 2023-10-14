8a9ab9ee18105e1b902a9460da24b5e433af779c
Pijar Pahlawan <pijarpahlawan@students.amikom.ac.id>
2023-10-14 10:06:56 +0700

    modify and adding additional api endpoints

--- src/scripts/data/api-endpoint.js
+++ src/scripts/data/api-endpoint.js
@@ -1,5 +1,13 @@
+import API_CONFIG from "../config/api-config";
+
 const API_ENDPOINTS = {
-    RESTAURANT_LIST: "./data/DATA.json",
+    RESTAURANT_LIST: `${API_CONFIG.BASE_URL}list`,
+    RESTAURANT_DETAIL: (id) => `${API_CONFIG.BASE_URL}detail/${id}`,
+    RESTAURANT_REVIEW: `${API_CONFIG.BASE_URL}review`,
+    RESTAURANT_SMALL_IMAGE: (pictureId) =>
+        `${API_CONFIG.BASE_SMALL_IMAGE_URL}${pictureId}`,
+    RESTAURANT_MEDIUM_IMAGE: (pictureId) =>
+        `${API_CONFIG.BASE_MEDIUM_IMAGE_URL}${pictureId}`,
 };
 
 export default API_ENDPOINTS;
