b551a9c80bfb7ac5758790b49f4c5c12edef036a
Pijar Pahlawan <pijarpahlawan@students.amikom.ac.id>
2023-10-14 13:04:33 +0700

    create favorite page

---
+++ src/scripts/views/pages/favorite-page.js
@@ -0,0 +1,13 @@
+const FavoritePage = {
+    async render() {
+        return `
+            <restaurant-list title="Your Favorite Restaurant"></restaurant-list>
+        `;
+    },
+
+    async afterRender() {
+        // TODO: display favorite restaurant
+    },
+};
+
+export default FavoritePage;
