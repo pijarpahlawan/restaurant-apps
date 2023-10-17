7e365be41f6aa07428de07c16f6208f987f0a619
Pijar Pahlawan <pijarpahlawan@students.amikom.ac.id>
2023-10-17 15:51:45 +0700

    create review item component

--- src/scripts/views/components/index.js
+++ src/scripts/views/components/index.js
@@ -2,3 +2,4 @@ import "./brand-logo";
 import "./hamburger-button";
 import "./restaurant-item";
 import "./restaurant-list";
+import "./review-item";
---
+++ src/scripts/views/components/review-item.js
@@ -0,0 +1,26 @@
+import template from "./templates/review-item.html";
+import "./styles/review-item.scss";
+
+class ReviewItem extends HTMLElement {
+    #review = null;
+
+    set review(value) {
+        this.#review = value;
+        this.render();
+    }
+
+    render() {
+        this.innerHTML = template;
+        this.className = "restaurant-review";
+
+        // get element
+        const headerElement = this.querySelector("#customerReviewHeader");
+        const contentElement = this.querySelector("#customerReviewContent");
+
+        // set attribute value
+        headerElement.innerHTML = `${this.#review.name} @${this.#review.date}`;
+        contentElement.innerHTML = this.#review.review;
+    }
+}
+
+customElements.define("review-item", ReviewItem);
