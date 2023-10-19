72d23ff61e5af5ff774fae9445c98e7c79353df1
Pijar Pahlawan <pijarpahlawan@students.amikom.ac.id>
2023-10-19 12:00:44 +0700

    remove error handler from data source

--- src/scripts/data/data-source.js
+++ src/scripts/data/data-source.js
@@ -1,39 +1,27 @@
 import API_ENDPOINTS from "./api-endpoint";
 
 async function getAllRestaurant() {
-    try {
-        const response = await fetch(API_ENDPOINTS.RESTAURANT_LIST);
-        const responseJson = await response.json();
-        return responseJson.restaurants;
-    } catch (error) {
-        return error.message;
-    }
+    const response = await fetch(API_ENDPOINTS.RESTAURANT_LIST);
+    const responseJson = await response.json();
+    return responseJson.restaurants;
 }
 
 async function getRestaurantDetail(id) {
-    try {
-        const response = await fetch(API_ENDPOINTS.RESTAURANT_DETAIL(id));
-        const responseJson = await response.json();
-        return responseJson.restaurant;
-    } catch (error) {
-        return error.message;
-    }
+    const response = await fetch(API_ENDPOINTS.RESTAURANT_DETAIL(id));
+    const responseJson = await response.json();
+    return responseJson.restaurant;
 }
 
 async function addRestaurantReview(review) {
-    try {
-        const response = await fetch(API_ENDPOINTS.RESTAURANT_REVIEW, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(review),
-        });
-        const responseJson = await response.json();
-        return responseJson;
-    } catch (error) {
-        return error.message;
-    }
+    const response = await fetch(API_ENDPOINTS.RESTAURANT_REVIEW, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(review),
+    });
+    const responseJson = await response.json();
+    return responseJson;
 }
 
 export { getAllRestaurant, getRestaurantDetail, addRestaurantReview };
