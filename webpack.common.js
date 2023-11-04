7289e17756991e8d661d4345d16f6dfe394afa0c
Pijar Pahlawan <pijarpahlawan@students.amikom.ac.id>
2023-11-04 23:24:27 +0700

    delete unnecessary things

--- webpack.common.js
+++ webpack.common.js
@@ -1,125 +1,98 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
-const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
-    entry: {
-        components: path.resolve(
-            __dirname,
-            "src/scripts/views/components/index.js"
-        ),
-        index: {
-            dependOn: "components",
-            import: path.resolve(__dirname, "src/scripts/index.js"),
-        },
+  entry: {
+    components: path.resolve(
+      __dirname,
+      'src/scripts/views/components/index.js',
+    ),
+    index: {
+      dependOn: 'components',
+      import: path.resolve(__dirname, 'src/scripts/index.js'),
     },
-    output: {
-        filename: "[name].bundle.js",
-        path: path.resolve(__dirname, "dist"),
-        clean: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.s?css$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-            },
-            {
-                test: /\.(html|svg)$/,
-                type: "asset/source",
-                exclude: path.resolve(__dirname, "src/index.html"),
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: "index.html",
-            template: path.resolve(__dirname, "src/index.html"),
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                {
-                    from: path.resolve(__dirname, "public/"),
-                    to: path.resolve(__dirname, "dist/"),
-                },
-            ],
-        }),
-        new MiniCssExtractPlugin({
-            filename: "[name].css",
-        }),
-        new WorkboxWebpackPlugin.GenerateSW({
-            swDest: "./sw.bundle.js",
-            runtimeCaching: [
-                {
-                    urlPattern: ({ url }) =>
-                        url.href.startsWith(
-                            "https://restaurant-api.dicoding.dev/"
-                        ),
-                    handler: "StaleWhileRevalidate",
-                    options: {
-                        cacheName: "restaurant-data",
-                        expiration: {
-                            maxAgeSeconds: 60 * 60 * 24 * 7,
-                        },
-                    },
-                },
-            ],
-        }),
+  },
+  output: {
+    filename: '[contenthash]-[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(html|svg)$/,
+        type: 'asset/source',
+        exclude: path.resolve(__dirname, 'src/index.html'),
+      },
     ],
-    optimization: {
-        splitChunks: {
-            chunks: "all",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'src/index.html'),
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'public/'),
+          to: path.resolve(__dirname, 'dist/'),
         },
-        minimize: true,
-        minimizer: [
-            new CssMinimizerPlugin(),
-            new TerserPlugin({
-                terserOptions: {
-                    format: {
-                        comments: false,
-                    },
-                },
-                extractComments: false,
-            }),
-            new ImageMinimizerPlugin({
-                minimizer: {
-                    implementation: ImageMinimizerPlugin.imageminMinify,
-                    options: {
-                        plugins: [
-                            ["jpegtran", { progressive: true }],
-                            [
-                                "svgo",
-                                {
-                                    plugins: [
-                                        {
-                                            name: "preset-default",
-                                            params: {
-                                                overrides: {
-                                                    removeViewBox: false,
-                                                    addAttributesToSVGElement: {
-                                                        params: {
-                                                            attributes: [
-                                                                {
-                                                                    xmlns: "http://www.w3.org/2000/svg",
-                                                                },
-                                                            ],
-                                                        },
-                                                    },
-                                                },
-                                            },
-                                        },
-                                    ],
-                                },
-                            ],
-                        ],
-                    },
-                },
-            }),
-        ],
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[contenthash]-[name].css',
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      swDest: './sw.bundle.js',
+      runtimeCaching: [
+        {
+          urlPattern: ({ url }) =>
+            url.href.startsWith('https://restaurant-api.dicoding.dev/'),
+          handler: 'StaleWhileRevalidate',
+          options: {
+            cacheName: 'restaurant-data',
+            expiration: {
+              maxAgeSeconds: 60 * 60 * 24 * 7,
+            },
+          },
+        },
+      ],
+    }),
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
     },
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
+      new TerserPlugin({
+        terserOptions: {
+          format: {
+            comments: false,
+          },
+        },
+        extractComments: false,
+      }),
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.svgoMinify,
+          options: {
+            multipass: true,
+            plugins: ['preset-default'],
+          },
+        },
+      }),
+    ],
+  },
 };
