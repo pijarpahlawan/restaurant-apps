b1845136f48bb080e75e721a2d0089e4dc3f9771
Pijar Pahlawan <pijarpahlawan@students.amikom.ac.id>
2023-11-06 01:00:22 +0700

    create exit way without emit error

--- src/scripts/data/favorite-restaurant-idb.js
+++ src/scripts/data/favorite-restaurant-idb.js
@@ -11,6 +11,8 @@ const dbPromise = openDB(DB_CONFIG.DB_NAME, DB_CONFIG.DB_VERSION, {
 
 const FavoriteRestaurantIdb = {
   async getRestaurant(id) {
+    if (!id) return Promise;
+
     return (await dbPromise).get(DB_CONFIG.OBJECT_STORE_NAME, id);
   },
 
@@ -19,10 +21,14 @@ const FavoriteRestaurantIdb = {
   },
 
   async putRestaurant(restaurant) {
+    if (!Object.hasOwn(restaurant, 'id')) return Promise;
+
     return (await dbPromise).put(DB_CONFIG.OBJECT_STORE_NAME, restaurant);
   },
 
   async deleteRestaurant(id) {
+    if (!id) return Promise;
+
     return (await dbPromise).delete(DB_CONFIG.OBJECT_STORE_NAME, id);
   },
 };
