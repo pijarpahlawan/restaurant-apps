4e1dee89d4b718dea2ffa2c0d5f5d8b7c6afd206
Pijar Pahlawan <pijarpahlawan@students.amikom.ac.id>
2023-11-07 09:16:04 +0700

    finishing test for liking restaurant scenario

--- e2e/liking-restaurant.test.js
+++ e2e/liking-restaurant.test.js
@@ -1,7 +1,34 @@
+const { pause } = require('codeceptjs');
+const assert = require('assert');
+
 Feature('Liking Restaurant');
 
-Before(({ I }) => {
+Scenario('showing empty liked restaurant', ({ I }) => {
   I.amOnPage('/#/favorite');
+  I.see('Your Favorite Restaurant', '#restaurantListTitle');
+  I.see('Restaurant not found', '#restaurantListNotFound');
 });
 
-Scenario('test something', ({ I }) => {});
+Scenario('liking one restaurant', async ({ I }) => {
+  I.amOnPage('/');
+
+  I.seeElement('.restaurant-item');
+
+  const firstRestaurant = locate('.restaurant-item').first();
+
+  I.click(firstRestaurant);
+
+  const restaurantName = await I.grabTextFrom('#restaurantName');
+
+  I.seeElement('#favoriteButton');
+  I.click('#favoriteButton');
+
+  I.amOnPage('/#/favorite');
+  I.seeElement('.restaurant-item');
+
+  I.click(locate('.restaurant-item').first());
+
+  const likedRestaurantName = await I.grabTextFrom('#restaurantName');
+
+  assert.strictEqual(restaurantName, likedRestaurantName);
+});
